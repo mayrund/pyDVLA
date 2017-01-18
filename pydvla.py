83a63ba87f22acc2259ab779dbde5d737b0a410a
Mayrun <mayrun@gmail.com>
2017-01-18 23:03:37 +0100

    Add files via upload

---
+++ pydvla.py
@@ -0,0 +1,29 @@
+###########################
+# DVLA Vehicle MOT Status #
+##########################
+# Mayrun Digmi 2017
+
+# Please install BeautifulSoup4 and Requests packages
+from sys import argv
+from bs4 import BeautifulSoup
+import requests
+
+formUrl = 'https://vehicleenquiry.service.gov.uk/ViewVehicle'
+
+def getcar(manufacturer, licenseplate):
+  resp = requests.post(formUrl, data = { 'Vrm': licenseplate, 'Make': manufacturer}, headers = {"Content-type": "application/x-www-form-urlencoded"} )
+  html = resp.content
+  soup = BeautifulSoup(html, 'html.parser')
+  ul = soup.find('ul', {'class':"ul-data"}).find_all("li")
+  for child in ul:
+    span = child.find("span")
+    strong = child.find("strong")
+    print span.text + ": " + strong.text
+
+if __name__ == '__main__':
+    args = argv[1:]
+    if len(args) is 2:
+        print 'Downloading vehcile information from DVLA...\n'
+        getcar(*args[:2])
+    else:
+        print "Usage: {0} manufacturer licenseplate".format(argv[0])
